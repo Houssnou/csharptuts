400234516468d1dfe28ae34404348efb26f4be32
Houssnou <hosni.moubarak@gmail.com>
2019-04-14 00:19:27 -0400

    CSharp Tutos

---
+++ 05/csharptut/csharptut/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharptut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Area of Rectangle : {0}", ShapeMath.GetArea("rectangle", 5, 6));
+
+            int? randNum = null;
+
+            if (randNum == null)
+            {
+                Console.WriteLine("randNum is null");
+            }
+
+            if (!randNum.HasValue)
+            {
+                Console.WriteLine("randNum is null");
+            }
+
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ 06/CSharptuT/CSharptuT/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharptuT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Animal();
+
+            cat.SetName("Whiskers");
+            cat.Sound = "Meow";
+            Console.WriteLine("The cat is named {0} and says {1}", cat.GetName(), cat.Sound);
+
+            cat.Owner = "Jason";
+            Console.WriteLine("{0} owner is {1}", cat.GetName(), cat.Owner);
+
+
+            Console.WriteLine("{0} shelter is {1}", cat.GetName(), cat.idNum);
+
+            Animal fox = new Animal("Oscar", "Rawrrrr");
+
+            Console.WriteLine("# of Animals : {0}", Animal.NumOfAnimals);
+
+            Console.ReadLine();
+        }
+    }
+}
